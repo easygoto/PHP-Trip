f23749979c0e7f9a480b5037c1e8023421e5efe0
trink <treelink@126.com>
2019-04-26 00:01:08 +0800

    Singleton PSR2

--- src/Singleton/Config/Config.php
+++ src/Singleton/Config/Config.php
@@ -3,25 +3,31 @@
 
 namespace Trink\Dp\Singleton\Config;
 
-
 /**
  * @property array      db
  * @property mixed|null test
  */
-class Config {
-
+class Config
+{
     private static $instance;
-    private        $props = [];
 
-    private function __construct() { }
+    private $props = [];
 
-    private function __clone() { }
+    private function __construct()
+    {
+    }
 
-    public function __get($name) {
+    private function __clone()
+    {
+    }
+
+    public function __get($name)
+    {
         return $this->props[$name] ?? null;
     }
 
-    public static function instance() {
+    public static function instance()
+    {
         if (! (self::$instance instanceof self)) {
             self::$instance        = new self();
             self::$instance->props = require_once __DIR__ . '/config/config.php';
--- src/Singleton/Config/config/db.php
+++ src/Singleton/Config/config/db.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
+    'host' => 'localhost',
     'password' => '123123',
     'database' => 'test',
     'port' => '3306',
