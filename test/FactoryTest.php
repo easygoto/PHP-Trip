35cee53629a65fd5f115b63805606deb375485e1
trink <treelink@126.com>
2019-04-25 10:01:02 +0800

    工厂模式测试

--- test/FactoryTest.php
+++ test/FactoryTest.php
@@ -3,11 +3,13 @@
 
 use PHPUnit\Framework\TestCase;
 use Trink\Dp\Factory\Demo\BloggsCommsManager;
+use Trink\Dp\Factory\Electronics\Computer;
 use Trink\Dp\Factory\Electronics\Computer\AIO;
 use Trink\Dp\Factory\Electronics\Computer\Laptop;
 use Trink\Dp\Factory\Electronics\Computer\PC;
 use Trink\Dp\Factory\Electronics\Computer\Tablet;
 use Trink\Dp\Factory\Electronics\Electronics;
+use Trink\Dp\Factory\Electronics\Phone;
 use Trink\Dp\Factory\Electronics\Phone\Andriod;
 use Trink\Dp\Factory\Electronics\Phone\IOS;
 use Trink\Dp\Factory\Electronics\Phone\Symbian;
@@ -26,72 +28,86 @@ class FactoryTest extends TestCase {
     }
 
     /**
+     * @test
      * @group electronics
      * @group computer
      */
-    function testAIO() {
+    function aio() {
         $aio = Electronics::computer('AIO');
-        $aio->run()->play()->close();
         $this->assertTrue($aio instanceof AIO);
+        $this->assertTrue($aio instanceof Computer);
+        $aio->run()->play()->close();
     }
 
     /**
+     * @test
      * @group electronics
      * @group computer
      */
-    function testLaptop() {
+    function laptop() {
         $laptop = Electronics::computer('Laptop');
-        $laptop->run()->play()->close();
         $this->assertTrue($laptop instanceof Laptop);
+        $this->assertTrue($laptop instanceof Computer);
+        $laptop->run()->play()->close();
     }
 
     /**
+     * @test
      * @group electronics
      * @group computer
      */
-    function testPC() {
+    function pc() {
         $pc = Electronics::computer('PC');
-        $pc->run()->play()->close();
         $this->assertTrue($pc instanceof PC);
+        $this->assertTrue($pc instanceof Computer);
+        $pc->run()->play()->close();
     }
 
     /**
+     * @test
      * @group electronics
      * @group computer
      */
-    function testTablet() {
+    function tablet() {
         $tablet = Electronics::computer('Tablet');
-        $tablet->run()->play()->close();
         $this->assertTrue($tablet instanceof Tablet);
+        $this->assertTrue($tablet instanceof Computer);
+        $tablet->run()->play()->close();
     }
 
     /**
+     * @test
      * @group electronics
      * @group phone
      */
-    function testIOS() {
+    function ios() {
         $ios = Electronics::phone('IOS');
-        $ios->open()->call();
         $this->assertTrue($ios instanceof IOS);
+        $this->assertTrue($ios instanceof Phone);
+        $ios->open()->call();
     }
 
     /**
+     * @test
      * @group electronics
      * @group phone
      */
-    function testAndriod() {
+    function andriod() {
         $andriod = Electronics::phone('Andriod');
-        $andriod->open()->call();
         $this->assertTrue($andriod instanceof Andriod);
+        $this->assertTrue($andriod instanceof Phone);
+        $andriod->open()->call();
     }
 
     /**
+     * @test
      * @group electronics
      * @group phone
      */
-    function testSymbian() {
+    function symbian() {
         $symbian = Electronics::phone('Symbian');
-        $symbian->open()->call();
         $this->assertTrue($symbian instanceof Symbian);
+        $this->assertTrue($symbian instanceof Phone);
+        $symbian->open()->call();
     }
 }
