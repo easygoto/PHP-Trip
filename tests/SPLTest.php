2fad4a16325563a6e460c99d49742372cef9eb60
trink <treelink@126.com>
2019-05-26 00:01:03 +0800

    spl

--- tests/SPLTest.php
+++ tests/SPLTest.php
@@ -3,7 +3,11 @@
 
 namespace Test\Demo;
 
+use AppendIterator;
+use ArrayIterator;
 use ArrayObject;
+use FilesystemIterator;
+use MultipleIterator;
 use PHPUnit\Framework\TestCase;
 use SplDoublyLinkedList;
 use SplPriorityQueue;
@@ -134,7 +138,7 @@ class SPLTest extends TestCase
         ];
 
         $obj = new ArrayObject($fruit_list);
-        $it = $obj->getIterator();
+        $it  = $obj->getIterator();
         foreach ($it as $key => $value) {
             print "{$key} : {$value}\n";
         }
@@ -167,7 +171,70 @@ class SPLTest extends TestCase
             $it->next();
         }
 
+        $this->assertTrue(true);
+    }
 
+    /** @test */
+    public function appendIterator()
+    {
+        $list1 = [1, 2, 3];
+        $list2 = ['a', 'b', 'c'];
+
+        $it1 = new ArrayIterator($list1);
+        $it2 = new ArrayIterator($list2);
+
+        $it = new AppendIterator();
+        $it->append($it1);
+        $it->append($it2);
+
+        while ($it->valid()) {
+            print "{$it->key()} : {$it->current()}\n";
+            $it->next();
+        }
+
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function multipleIterator()
+    {
+        $id_list   = [1, 2, 3];
+        $name_list = ['trink', 'easy', 'hello'];
+        $age_list  = [20, 21, 22];
+
+        $id_it   = new ArrayIterator($id_list);
+        $name_it = new ArrayIterator($name_list);
+        $age_it  = new ArrayIterator($age_list);
+
+        // 合并数组，保留键值
+        $mit = new MultipleIterator(MultipleIterator::MIT_KEYS_ASSOC);
+        $mit->attachIterator($id_it, 'id');
+        $mit->attachIterator($name_it, 'name');
+        $mit->attachIterator($age_it, 'age');
+
+        while ($mit->valid()) {
+            print_r($mit->current());
+            $mit->next();
+        }
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function fileSystemIterator()
+    {
+        $path = dirname(__DIR__);
+        $fit  = new FilesystemIterator($path);
+        while ($fit->valid()) {
+            printf(
+                "%-30s%-8s%15s  %s\n",
+                date('Y-m-d H:i:s', $fit->getMTime()),
+                ($fit->isDir() ? '<DIR>' : '<FILE>'),
+                number_format($fit->getSize()),
+                $fit->getFilename()
+            );
+            $fit->getPathname();
+            $fit->next();
+        }
         $this->assertTrue(true);
     }
 }
