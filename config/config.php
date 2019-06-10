9ecc4a60ac9c7e1379656b82dfee536a7def8856
trink <treelink@126.com>
2019-06-10 22:22:48 +0800

    config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'db'    => require_once 'db.php',
-    'redis' => require_once 'redis.php',
+    'db'     => require_once 'db.php',
+    'redis'  => require_once 'redis.php',
+    'rabbit' => require_once 'rabbit.php',
 ];
---
+++ config/rabbit.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'host'   => '127.0.0.1',
+    'port'   => '5672',
+    'user'   => 'demo',
+    'pass'   => '123123',
+    'v_host' => 'demo',
+
+    'e_name'    => 'e_trink', //交换机名
+    'q_name'    => 'q_trink', //无需队列名
+    'key_route' => 'key', //路由key
+];
