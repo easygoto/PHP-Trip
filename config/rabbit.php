56ce773d1120c5402842679f0c929adaffdb131d
trink <treelink@126.com>
2019-06-10 23:10:17 +0800

    config

--- config/rabbit.php
+++ config/rabbit.php
@@ -1,13 +1,9 @@
 <?php
 
 return [
-    'host'   => '127.0.0.1',
-    'port'   => '5672',
-    'user'   => 'demo',
-    'pass'   => '123123',
-    'v_host' => 'demo',
-
-    'e_name'    => 'e_trink', //交换机名
-    'q_name'    => 'q_trink', //无需队列名
-    'key_route' => 'key', //路由key
+    'host'     => '127.0.0.1',
+    'port'     => '5672',
+    'login'    => 'demo',
+    'password' => '123123',
+    'vhost'    => 'demo',
 ];
