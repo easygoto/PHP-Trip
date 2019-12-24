985e0d2cb0ab6559e9a6177a1457ea801657af9c
trink <treelink@126.com>
2019-12-24 18:46:18 +0800

    tcp socket

--- tests/Net/SocketTest.php
+++ tests/Net/SocketTest.php
@@ -6,87 +6,52 @@ namespace Test\Net;
 class SocketTest extends TestCase
 {
     /** @test */
-    public function httpHead()
+    public function tcpServer()
     {
-        echo $this->httpRequest(
-            $this->hostname(),
-            $this->returnJsonPath(),
-            'HEAD'
-        );
-        $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function httpGet()
-    {
-        echo $this->httpRequest(
-            $this->hostname(),
-            $this->returnJsonPath(),
-            'GET',
-            http_build_query(['name' => '中文'])
-        );
-        $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function httpPost()
-    {
-        echo $this->httpRequest(
-            $this->hostname(),
-            $this->returnJsonPath(),
-            'POST',
-            json_encode(['name' => '中文']),
-            ['Content-Type: application/json']
-        );
-        $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function httpPatch()
-    {
-        echo $this->httpRequest(
-            $this->hostname(),
-            $this->returnJsonPath(),
-            'PATCH',
-            json_encode(['name' => '中文']),
-            ['Content-Type: application/json']
-        );
-        $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function httpDelete()
-    {
-        echo $this->httpRequest(
-            $this->hostname(),
-            $this->returnJsonPath(),
-            'DELETE',
-            json_encode(['name' => '中文']),
-            ['Content-Type: application/json']
-        );
-        $this->assertTrue(true);
-    }
-
-    public function httpRequest($host, $path, $method = 'GET', $data = '', $headers = [])
-    {
-        $conn = pfsockopen($host, 80, $errno, $errMsg, 30);
-        if ($errno == 0 && $conn) {
-            $http = "{$method} {$path} HTTP/1.1" . PHP_EOL;
-            $http .= "Host: {$host}" . PHP_EOL;
-            $http .= 'Connection: Close' . PHP_EOL;
-            if (in_array($method, ['POST', 'PUT', 'PATCH', 'DELETE'])) {
-                $http .= 'Content-Length: ' . strlen($data) . PHP_EOL;
-            }
-            $http .= implode(PHP_EOL, $headers) . PHP_EOL;
-            $http .= PHP_EOL . $data . PHP_EOL . PHP_EOL;
-            fputs($conn, $http);
+        $server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_bind($server, "192.168.0.173", 9696);
+        socket_listen($server);
+        $logFilename = __DIR__ . '/debug/' . date('Y_m_d_H_i_s') . '.log';
+        $clientLinkNum = 0;
+        while ($clientLinkNum++ < 5) {
+            $conn = socket_accept($server);
+            socket_getpeername($conn, $address, $port);
+            $uniqueKey = "{$address}:{$port}";
             $content = '';
-            while (!feof($conn)) {
-                $content .= fgets($conn);
+            while (true) {
+                $data = socket_read($conn, 1024);
+                if ($data === '') {
+                    break;
+                }
+                $content .= $data;
             }
-            return $content;
-        } else {
-            return "Error: $errMsg($errno)";
+
+            $logText = <<<LOG
+address: {$uniqueKey}
+content: {$content}
+\n
+LOG;
+            file_put_contents($logFilename, $logText, FILE_APPEND);
         }
+        socket_close($server);
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function tcpClient()
+    {
+        $linkNum = 5;
+        while ($linkNum-- > 0) {
+            $packageNum = 10;
+            $client = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            socket_connect($client, "192.168.0.173", 9696);
+            while ($packageNum-- > 0) {
+                $rand = rand(1e4, 1e5 - 1);
+                socket_write($client, "{$rand} ");
+                usleep(1e5);
+            }
+            socket_close($client);
+        }
+        $this->assertTrue(true);
     }
 }
