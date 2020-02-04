235ed54d9a1efdae35f5c364df3d2e289848a7cf
trink <treelink@126.com>
2020-02-04 19:07:41 +0800

    - `feat` 仿作微信聊天小应用

---
+++ public/demo/wechat/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Chat</title>
+    <link rel="stylesheet" href="/resource/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="we-app">
+        <header class="we-header">
+            <div class="pull-left app-name">微信</div>
+            <!--<div class="pull-right btn-group">
+                <span class="pull-right glyphicon glyphicon-plus"></span>
+                <span class="pull-right glyphicon glyphicon-search"></span>
+            </div>-->
+        </header>
+        <div class="msg-block">
+            <div class="msg-row">
+                <div class="avatar">
+                    <img src="" alt="">
+                </div>
+                <div class="chat-user">
+                    dage
+                </div>
+                <div class="chat-last-time">
+                    13:45
+                </div>
+                <div class="chat-intro">
+                    今天去
+                </div>
+            </div>
+        </div>
+        <footer class="we-footer">
+            <div class="nav-menu">1</div>
+            <div class="nav-menu">2</div>
+            <div class="nav-menu">3</div>
+            <div class="nav-menu">4</div>
+        </footer>
+    </div>
+</body>
+<script src="/resource/js/jquery.min.js"></script>
+<script src="/resource/js/bootstrap.min.js"></script>
+<script src="/resource/js/vue.min.js"></script>
+<script src="js/main.js"></script>
+</html>
